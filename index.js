edbcca33078f060cb2b6b0f1aea602d0f18a82f3
Ventsislav Dimitrov <Ventsislav.Dimitrov@ventsislav.dimitrov-mbp>
2020-09-16 15:58:24 +0300

    Added mongodb connection and global error handling

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const CONSTS = require('./util/constants');
-
-//Route files
-const vehicle = require('./routes/vehicle');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+//Route files
+const vehicle = require('./routes/vehicle');
 
 const app = express();
 
@@ -20,9 +23,16 @@ if (process.env.NODE_ENV === CONSTS.ENVS.DEV) {
 app.use('/api/v1/vehicles', vehicle);
 
 const PORT = process.env.PORT || 3000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Fuel Tracker API working on ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
+
+// Handle unhandled rejections
+process.on('unhandledRejection', err => {
+  console.log(`ERROR: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
